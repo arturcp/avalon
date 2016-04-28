60dfcaa4e1e7bcf576db019fa361e0cb4b8d9a9e
Artur <artur.prado@gmail.com>
2016-04-27 23:38:11 -0300

    Fix Quests model sintaxe

--- app/models/quests.rb
+++ app/models/quests.rb
@@ -2,12 +2,12 @@ class Quests
   attr_reader :players_count
 
   PARTY_SIZE = {
-    5:  [2, 3, 2, 3, 3],
-    6:  [2, 3, 4, 3, 4],
-    7:  [2, 3, 3, 4, 4],
-    8:  [3, 4, 4, 5, 5],
-    9:  [3, 4, 4, 5, 5],
-    10: [3, 4, 4, 5, 5]
+    5 =>  [2, 3, 2, 3, 3],
+    6 =>  [2, 3, 4, 3, 4],
+    7 =>  [2, 3, 3, 4, 4],
+    8 =>  [3, 4, 4, 5, 5],
+    9 =>  [3, 4, 4, 5, 5],
+    10 => [3, 4, 4, 5, 5]
   }
 
   def initialize(players_count)
@@ -34,14 +34,14 @@ class Quests
   end
 
   def party_size(quest_number)
-    PARTY_SIZE[players_count][mission_number - 1]
+    PARTY_SIZE[players_count][quest_number - 1]
   end
 
   def success_count
-    @quests.reduce(0) { |quest| quest.succeed? 1 : 0 }
+    @quests.reduce(0) { |quest| quest.succeed? ? 1 : 0 }
   end
 
   def failure_count
-    @quests.reduce(0) { |quest| quest.failed? 1 : 0 }
+    @quests.reduce(0) { |quest| quest.failed? ? 1 : 0 }
   end
 end
